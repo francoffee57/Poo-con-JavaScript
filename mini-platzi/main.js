38df09af417dece6e862c6ef9873f0960cf39ba9
Francisco García <francoffee57@gmail.com>
2021-08-20 22:32:04 -0500

    Prototipos usando la sintaxis de clases

--- mini-platzi/main.js
+++ mini-platzi/main.js
@@ -14,20 +14,51 @@ console.log(nathaly)
 
 
 //Prototipos
-function Studens(name, age, cursosAprobados){
+function Students(name, age, cursosAprobados){
     this.name = name,
     this.age = age,
     this.cursosAprobados = cursosAprobados
 }
 
-Studens.prototype.aprobarCursos = function (newCursoAprobado){
+Students.prototype.aprobarCursos = function (newCursoAprobado){
     this.cursosAprobados.push(newCursoAprobado);
 }
 
-const alejandra = new Studens(
+
+// Instancia de prototipos
+const alejandra = new Students(
     'Alejandra', 
     29, 
     ['Figma', 'Introducción al diseño UI y UX']
 )
 alejandra.aprobarCursos(`Illustrator`)
 console.log(alejandra)
+
+//Prototipos usando la sintaxis de clases
+//En ves de pasar cada parametro podemos enviar un objeto literal que tenga la información que necesitamos. Tambien podemos inicializarlos
+class Students2{
+    constructor({
+        name,
+        age,
+        email,
+        cursosAprobados = [],
+    }){
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.cursosAprobados = cursosAprobados
+    }
+
+    aprobarCurso(nuevoCursito) {
+        this.cursosAprobados.push(nuevoCursito);
+      }
+}
+
+const pablito = new Students2({
+    name: 'Pabito',
+    age: 23,
+    email: 'pablito123@email.com'
+})
+console.log(pablito)
+pablito.aprobarCurso('Curso de terminal y linea de comandos')
+console.log(pablito)
